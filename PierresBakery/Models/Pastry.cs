cd6c2db28e013e9c22880dd61a1c3076cc3dbd4c
Paige Tiedeman <paigetiedeman@gmail.com>
2021-09-24 11:58:34 -0700

    test GetPastryCost passed

--- PierresBakery/Models/Pastry.cs
+++ PierresBakery/Models/Pastry.cs
@@ -6,10 +6,12 @@ namespace PierresBakery.Models
   public class Pastry
   {
     public int PastryCount {get; set; }
+    public int PastryCost {get; }
 
     public Pastry(int pastryCount)
     {
       PastryCount = pastryCount;
+      PastryCost = 2;
     }
   }
 }
