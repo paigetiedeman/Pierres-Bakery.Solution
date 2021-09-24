556f544bd0e78fcdd224a2b8db75b457e1b06567
Paige Tiedeman <paigetiedeman@gmail.com>
2021-09-24 09:15:03 -0700

    first test for constructor Bread passing

---
+++ PierresBakery.Tests/ModelTests/BreadTests.cs
@@ -0,0 +1,18 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System;
+using PierresBakery.Models;
+
+namespace PierresBakery.Tests
+{
+  [TestClass]
+  public class BreadTests
+  {
+    [TestMethod]
+    public void BreadConstructor_CreatesInstanceOfBread_Bread()
+    {
+      Bread newBread = new Bread();
+      Assert.AreEqual(typeof(Bread), newBread.GetType());
+    }
+  }
+}
