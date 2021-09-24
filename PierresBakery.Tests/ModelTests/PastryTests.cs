7e42c1af4c37c49d9867fc30698b4ecd8b42c7f9
Paige Tiedeman <paigetiedeman@gmail.com>
2021-09-24 11:38:18 -0700

    test added for Pastry constructor

--- PierresBakery.Tests/ModelTests/PastryTests.cs
+++ PierresBakery.Tests/ModelTests/PastryTests.cs
@@ -8,11 +8,11 @@ namespace PierresBakery.Tests
   [TestClass]
   public class PastryTests
   {
-    // [TestMethod]
-    // public void NameOfMethodWeAreTesting_DescriptionOfBehavior_ExpectedReturnValue()
-    // {
-    //   // any necessary logic to prep for test; instantiating new classes, etc.
-    //   Assert.AreEqual(EXPECTED RESULT, CODE TO TEST);
-    // }
+    [TestMethod]
+    public void PastryConstructor_CreatesInstanceOfPastry_Pastry()
+    {
+      Pastry newPastry = new Pastry();
+      Assert.AreEqual(typeof(Pastry), newPastry.GetType());
+    }
   }
 }
