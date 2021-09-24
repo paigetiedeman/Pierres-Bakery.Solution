314a8afa2c43a05a3e624ab54ee2a0af7c06413a
Paige Tiedeman <paigetiedeman@gmail.com>
2021-09-24 09:39:21 -0700

    test added for BreadCost

--- PierresBakery.Tests/ModelTests/BreadTests.cs
+++ PierresBakery.Tests/ModelTests/BreadTests.cs
@@ -14,5 +14,13 @@ namespace PierresBakery.Tests
       Bread newBread = new Bread();
       Assert.AreEqual(typeof(Bread), newBread.GetType());
     }
+    [TestMethod]
+    public void GetBreadCost_ReturnsBreadCost_Int()
+    {
+      int count = 1;
+      Bread newBread = new Bread(count);
+      int cost = newBread.BreadCost;
+      Assert.AreEqual(count, cost);
+    }
   }
 }
